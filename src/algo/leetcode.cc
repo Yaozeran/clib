083286fcce5886bae345579f3b38f0a3d76fb019
Yaozeran <zeran.yao@icloud.com>
2026-03-26 23:28:11 +0800

    updated dfs, bfs, sliding windows qs

--- src/algo/leetcode.cc
+++ src/algo/leetcode.cc
@@ -1,9 +1,12 @@
 #include "leetcode.h"
 
+
 namespace leetcode
 {
 
+
 /* set zero */
+
 
 vector<int> set0::two_sums_q1(vector<int>& nums, int target) {
   vector<int> ret;
@@ -21,11 +24,13 @@ vector<int> set0::two_sums_q1(vector<int>& nums, int target) {
   return ret;
 }
 
+
 int set0::single_number_q136(vector<int>& nums) {
   int n;
   for (int i = 0; i < nums.size(); ++i) n ^= nums[i];
   return n;
 }
+
 
 int set0::majority_element_q169(vector<int>& nums) {
   unordered_map<int, int> map;
@@ -43,13 +48,52 @@ int set0::majority_element_q169(vector<int>& nums) {
   return ret;
 }
 
+
+void dfs_q257(set0::tree_node* n, string& s, vector<string>& ret) {
+  int len = s.size();
+  s.append(to_string(n->val));
+  if (!(n->left || n->right)) {
+    ret.push_back(s);
+  } else {
+    s.append("->");
+    if (n->left) dfs_q257(n->left, s, ret);
+    if (n->right) dfs_q257(n->right, s, ret);
+  }
+  s.erase(len);
+  return;
+}
+vector<string> set0::binary_tree_path_q257(tree_node* n) {
+  vector<string> ret;
+  string s;
+  dfs_q257(n, s, ret);
+  return ret;
+}
+
+
+int set0::longest_substring_without_repeating_characters(string s) {
+  unordered_map<char, int> chars;
+  int i = 0, j = 0, ret = 0, temp = 0;
+  while (i != s.size()) {
+    auto iter = chars.find(s[i]);
+    if (iter != chars.end()) { 
+      j = max(j, iter->second + 1);
+      iter->second = i;
+    } else { chars.emplace(s[i], i); }
+    i++;
+    ret = max(ret, i - j);
+  }
+  return ret;
+}
+
+
 class set0::lru_cache_q146
 {
- public:
+public:
 
   class lru_linked_list
   {
-   public:
+  public:
+
     struct node {
       int key;
       int val;
@@ -58,7 +102,8 @@ class set0::lru_cache_q146
       node(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
     };
 
-   public: 
+  public: 
+
     lru_linked_list() : head_(nullptr), tail_(nullptr) {}
     
     ~lru_linked_list() {
@@ -115,13 +160,13 @@ class set0::lru_cache_q146
     node* head() { return head_; }
     node* tail() { return tail_; }
 
-   private:
+  private:
     node* head_;
     node* tail_;
   };
   typedef lru_linked_list::node node;
 
- public: 
+public: 
 
   lru_cache_q146(int capacity) : data_(), map_() {}
 
@@ -141,14 +186,123 @@ class set0::lru_cache_q146
     data_.pop_back();
   }
 
- private:
+private:
   
   lru_linked_list data_;
   unordered_map<int, node*> map_;
 
-};
+}; // set0::lru_cache_q146
+
+
+vector<vector<int>> set0::binary_tree_level_order_traversal_q102(tree_node* root) {
+  vector<vector<int>> ret{};
+  if (root == nullptr) return ret;
+  queue<tree_node*> nodes;
+  nodes.push(root);
+  while (!nodes.empty()) {
+    vector<int> level_vals;
+    int len = nodes.size();
+    for (int i = 0; i < len; ++i) {
+      tree_node* ptr = nodes.front();
+      level_vals.push_back(ptr->val);
+      if (ptr->left != nullptr) nodes.push(ptr->left);
+      if (ptr->right != nullptr) nodes.push(ptr->right);
+      nodes.pop();
+    }
+    ret.push_back(level_vals);
+  }
+  return ret;
+}
+
+
+void dfs_q113(set0::tree_node* n, int target, int temp, vector<int>& path, vector<vector<int>>& ret) {
+  if (n->left == nullptr && n->right == nullptr) {
+    if (temp + n->val != target) {
+      return;
+    } else {
+      path.push_back(n->val);
+      ret.push_back(path);
+      path.pop_back();
+      return;
+    }
+  } else {
+    path.push_back(n->val);
+    if (n->left != nullptr) { dfs_q113(n->left, target, temp + n->val, path, ret); }
+    if (n->right != nullptr) { dfs_q113(n->right, target, temp + n->val, path, ret); }
+    path.pop_back();
+    return;
+  }
+}
+vector<vector<int>> set0::path_sum_ii_q113(tree_node* root, int target) {
+  vector<vector<int>> ret;
+  if (root == nullptr) return ret;
+  vector<int> path;
+  dfs_q113(root, target, 0, path, ret);
+  return ret;
+}
+
+
+vector<int> set0::lexicographical_numbers_q386(int n) { 
+  vector<int> ret; // n = 120: 1, 10, 100, 101, ..., 11, 110, 111, 112, 113, 114, ..., 12, 120, 13, 14, ..., 20
+  int cnt = 1;
+  for (int i = 0; i < n; ++i) {
+    ret.push_back(cnt);
+    if (cnt * 10 <= n) {
+      cnt *= 10;
+    } else {
+      if (cnt > n) cnt /= 10;
+      cnt++;
+      while (cnt % 10 == 0) { cnt /= 10; }
+    }
+  }
+  return ret;
+}
+
+
+vector<int> set0::find_all_anagrams_in_a_string_q438(string s, string p) {
+  if (s.size() < p.size()) return {};
+  vector<int> ret, scnt(26, 0), pcnt(26, 0);
+  for (int i = 0; i < p.size(); ++i) {
+    scnt[s[i] - 'a']++;
+    pcnt[p[i] - 'a']++;
+  }
+  if (scnt == pcnt) ret.push_back(0);
+  for (int i = p.size(); i < s.size(); ++i) {
+    scnt[s[i] - 'a']++;
+    int j = i - p.size();
+    scnt[s[j] - 'a']--;
+    if (scnt == pcnt) ret.push_back(j + 1);
+  }
+  return ret;
+}
+
+
+std::pair<int, int> dfs_q124(set0::tree_node* n) {
+  if (!n->left && !n->right) { return {n->val, n->val}; } 
+  int f, s; // the first is connected with only current node, the second is finished connecting from left to n to right
+  std::pair<int, int> l, r;
+  if (n->left) l = dfs_q124(n->left);
+  if (n->right) r = dfs_q124(n->right);
+  if (n->left && n->right) {
+    f = max({l.first + n->val, r.first + n->val, n->val});
+    s = max({l.second, r.second, n->val, l.first + n->val, r.first + n->val, l.first + n->val + r.first});
+  } else if (n->left) {
+    f = max(l.first + n->val, n->val);
+    s = max({l.second, n->val, l.first + n->val});
+  } else {
+    f = max(r.first + n->val, n->val);
+    s = max({r.second, n->val, r.first + n->val});
+  }
+  return {f, s};
+}
+int set0::binary_tree_maximum_path_sum_q124(tree_node* root) {
+  auto p = dfs_q124(root);
+  return max(p.first, p.second);
+}
+
 
 /* set one */
+
 
 linked_list_node<int>* set1::intersection_of_two_linked_list_q160(linked_list_node<int>* a, linked_list_node<int>* b) {
   linked_list_node<int>* ptr_a = a; 
@@ -164,6 +318,7 @@ linked_list_node<int>* set1::intersection_of_two_linked_list_q160(linked_list_no
   return ptr_a;
 }
 
+
 linked_list_node<int>* set1::reverse_linked_list_q206(linked_list_node<int>* n) {
   linked_list_node<int>* prev = nullptr;
   linked_list_node<int>* ptr = n;
@@ -175,6 +330,7 @@ linked_list_node<int>* set1::reverse_linked_list_q206(linked_list_node<int>* n) 
   }
   return ptr;
 }
+
 
 int set1::container_with_most_water_q11(vector<int>& height) {
   size_t i = 0, j = height.size() - 1;
@@ -190,6 +346,7 @@ int set1::container_with_most_water_q11(vector<int>& height) {
   }
   return ret;
 }
+
 
 int set1::trapping_rain_water_q42(vector<int>& height) {
   size_t i = 0, j = height.size() - 1;
@@ -208,7 +365,9 @@ int set1::trapping_rain_water_q42(vector<int>& height) {
   return ret;
 }
 
+
 /* set two */
+
 
 int set2::search_insert_position_q35(vector<int>& nums, int target) {
   int i = 0, j = nums.size() - 1;
@@ -222,6 +381,7 @@ int set2::search_insert_position_q35(vector<int>& nums, int target) {
   }
   return i;
 }
+
 
 bool set2::search_a_2d_matrix_q74(vector<vector<int>>& matrix, int target) {
   int top = 0, bot = matrix.size() - 1;
@@ -245,17 +405,22 @@ bool set2::search_a_2d_matrix_q74(vector<vector<int>>& matrix, int target) {
   return false;
 }
 
+
+/* set three */
+
+
 /* set four */
+
 
 int set4::climb_stairs_q70(int n) {
   if (n == 1 || n == 2) return n;
-  vector<int> dp(n + 1);
-  dp[1] = 1;
-  dp[2] = 2;
-  for (int i = 3; i < n + 1; ++i) {
-    dp[i] = dp[i - 1] + dp[i - 2];
+  int a = 1, b = 2;
+  for (int i = 2; i < n; ++i) {
+    int t = a + b;
+    a = b;
+    b = t;
   }
-  return dp[n];
+  return b;
 }
 
 vector<vector<int>> set4::pascals_triangle_q118(int n) {
@@ -278,10 +443,51 @@ vector<vector<int>> set4::pascals_triangle_q118(int n) {
 }
 
 int set4::house_robber_q198(vector<int>& nums) {
-  
+  if (nums.size() == 1) return nums[0];
+  if (nums.size() == 2) return max(nums[0], nums[1]);
+  vector<int> dp;
+  dp[0] = nums[0];
+  dp[1] = max(nums[0], nums[1]);
+  for (int i = 2; i < nums.size(); ++i) {
+    dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+  }
+  return dp[nums.size() - 1];
 }
 
+int set4::perfect_squares_q279(int n) {
+  if (n < 4) return n;
+  vector<int> dp(n + 1);
+  dp[0] = 0;
+  for (int i = 1; i < n + 1; ++i) {
+    dp[i] = i;
+    for (int j = 1; j * j < i + 1; ++j) {
+      dp[i] = min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+  return dp[n];
+}
+
+int set4::coin_change_q322(vector<int>& coins, int amount) {
+  vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, 0));
+  int inf = amount + 1;
+  for (int i = 0; i < amount + 1; ++i) {
+    dp[0][i] = inf;
+  }
+  for (int i = 0; i < coins.size(); ++i) {
+    for (int j = 1; j < amount + 1; ++j) {
+      if (coins[i] < amount) {
+        dp[i][j] = dp[i - 1][j];
+      } else {
+        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1] + 1);
+      }
+    }
+  }
+  return dp[coins.size() - 1][amount] == inf ? -1 : dp[coins.size() - 1][amount];
+}
+
+
 /* set five */
+
 
 int set5::best_time_to_buy_and_sell_stock_q121(vector<int>& prices) {
 
--- src/algo/leetcode.h
+++ src/algo/leetcode.h
@@ -1,62 +1,91 @@
 #pragma once
 
+
 #include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <algorithm>
 
 #include <vector>
 #include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
 
 #include "ds/list.h"
+
 
 namespace leetcode
 {
 
+
 using namespace std;
+
 
 enum class category {
   unknown, 
   pointers, divide_and_conquer, backtracking, dynamic_programming, greedy
 };
 
+
 template<category cat>
 class solutions { };
+
 
 /* miscellaneous */
 class set0 : public solutions<category::unknown>
 {
- public:
+public: // helper class
+  struct tree_node 
+  {
+    int val;
+    tree_node* left;
+    tree_node* right;
+    tree_node() : val(0), left(nullptr), right(nullptr) {}
+    tree_node(int x) : val(x), left(nullptr), right(nullptr) {}
+    tree_node(int x, tree_node *left, tree_node *right) : val(x), left(left), right(right) {}
+  };
+public: // simple
   vector<int> two_sums_q1(vector<int>& nums, int target);
   int single_number_q136(vector<int>& nums);
-  int majority_element_q169(vector<int>& nums);
- public: 
+  int majority_element_q169(vector<int>& nums); 
+  vector<string> binary_tree_path_q257(tree_node* n);
+public: // medium
+  int longest_substring_without_repeating_characters(string s);
   class lru_cache_q146;
- public:
-  
+  vector<vector<int>> binary_tree_level_order_traversal_q102(tree_node* root);
+  vector<vector<int>> path_sum_ii_q113(tree_node* root, int target);
+  vector<int> lexicographical_numbers_q386(int n);
+  vector<int> find_all_anagrams_in_a_string_q438(string s, string p);
+public: // hard
+  int binary_tree_maximum_path_sum_q124(tree_node* root);
 };
+
 
 /* */
 class set1 : public solutions<category::pointers>
 {
- public: 
+public: 
   linked_list_node<int>* intersection_of_two_linked_list_q160(linked_list_node<int>* a, linked_list_node<int>* b);
   linked_list_node<int>* reverse_linked_list_q206(linked_list_node<int>* n);
- public:
+public:
   int container_with_most_water_q11(vector<int>& height);
- public:
+public:
   int trapping_rain_water_q42(vector<int>& height);
 };
+
 
 /* */
 class set2 : public solutions<category::divide_and_conquer>
 {
- public:
+public:
   int search_insert_position_q35(vector<int>& nums, int target);
- public:
+public:
   bool search_a_2d_matrix_q74(vector<vector<int>>& matrix, int targe);
- public:
+public:
   
 };
+
 
 /* */
 class set3 : public solutions<category::backtracking>
@@ -64,21 +93,26 @@ class set3 : public solutions<category::backtracking>
 
 };
 
+
 /* */
 class set4 : public solutions<category::dynamic_programming>
 {
- public:
+public:
   int climb_stairs_q70(int n);
   vector<vector<int>> pascals_triangle_q118(int n);
- public:
+public:
   int house_robber_q198(vector<int>& nums);
+  int perfect_squares_q279(int n);
+  int coin_change_q322(vector<int>& coins, int amount);
 };
+
 
 /* */
 class set5 : public solutions<category::greedy>
 {
- public:
+public:
   int best_time_to_buy_and_sell_stock_q121(vector<int>& prices);
 };
 
-} /* namespace leetcode */
+
+} // namespace leetcode
